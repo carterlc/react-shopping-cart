ad84ad44d3700cfd7a3a8e7cba23809644aa488e
Carter Clifford <carterlc@live.ca>
2023-03-15 22:08:37 -0600

    Styled shopping cart buttons and close icon.

--- src/App.js
+++ src/App.js
@@ -36,108 +36,109 @@ function App() {
           <div className='cart-content'>
             <div className='cart-box'>
               <img src="https://www.switchbacktravel.com/sites/default/files/image_fields/Best%20Of%20Gear%20Articles/Snowboarding/Snowboard%20Jackets/Burton%20AK%202L%20Gore-Tex%20Swash%20snowboard%20jacket.jpg" alt="Snowboarding Jacket" />
-              <div className='details'>
+              <div className='cart-details'>
                 <div className='cart-product-title'>Snowboard Jacket</div>
                 <div className='cart-product-price'>$329 CAD</div>
                 <div className='input-and-icon'>
                   <input type="number" value="1" class="cart-quantity"></input>
-                  <i class='bx bx-trash'></i>
+                  <i class='bx bx-trash cart-remove'></i>
                 </div>
                 <div className='total'>
                   <div className='total-title'>Total</div>
-                  <div className='total-price'></div>
+                  <div className='total-price'>$0</div>
                 </div>
                 {/* Purchase */}
-                <button type="button" class="btn-buy">Buy Now</button>
                 <i class='bx bx-x' id="close-cart"></i>
               </div>
             </div>
+            <button type="button" class="btn-buy">Buy Now</button>
+            <button type="button" class="btn-close">Keep Shopping</button>
           </div>
         </div>
-   
-      {/* Product Grid */}
-      <section className='products'>
-        <h2>Products</h2>
-        <div className='product-grid'>
-          <div className='product'>
-            <img src="https://www.switchbacktravel.com/sites/default/files/image_fields/Best%20Of%20Gear%20Articles/Snowboarding/Snowboard%20Jackets/Burton%20AK%202L%20Gore-Tex%20Swash%20snowboard%20jacket.jpg" alt="Snowboarding Jacket" />
-            <h3>Snowboard Jacket</h3>
-            <p>Men White Dotted</p>
-            <div className='price-wrapper'>
+
+        {/* Product Grid */}
+        <section className='products'>
+          <h2>Products</h2>
+          <div className='product-grid'>
+            <div className='product'>
+              <img src="https://www.switchbacktravel.com/sites/default/files/image_fields/Best%20Of%20Gear%20Articles/Snowboarding/Snowboard%20Jackets/Burton%20AK%202L%20Gore-Tex%20Swash%20snowboard%20jacket.jpg" alt="Snowboarding Jacket" />
+              <h3>Snowboard Jacket</h3>
+              <p>Men White Dotted</p>
+              <div className='price-wrapper'>
+                <span className='price'>$329 CAD</span>
+                <button className='product-button'>Add to Cart</button>
+              </div>
+            </div>
+            <div className='product'>
+              <img></img>
+              <h3>Snowboard Jacket</h3>
+              <p>Men White Dotted</p>
               <span className='price'>$329 CAD</span>
-              <button className='product-button'>Add to Cart</button>
+              <button>Add to Cart</button>
+            </div>
+            <div className='product'>
+              <img></img>
+              <h3>Snowboard Jacket</h3>
+              <p>Men White Dotted</p>
+              <span className='price'>$329 CAD</span>
+              <button>Add to Cart</button>
+            </div>
+            <div className='product'>
+              <img></img>
+              <h3>Snowboard Jacket</h3>
+              <p>Men White Dotted</p>
+              <span className='price'>$329 CAD</span>
+              <button>Add to Cart</button>
+            </div>
+            <div className='product'>
+              <img></img>
+              <h3>Snowboard Jacket</h3>
+              <p>Men White Dotted</p>
+              <span className='price'>$329 CAD</span>
+              <button>Add to Cart</button>
+            </div>
+            <div className='product'>
+              <img></img>
+              <h3>Snowboard Jacket</h3>
+              <p>Men White Dotted</p>
+              <span className='price'>$329 CAD</span>
+              <button>Add to Cart</button>
             </div>
           </div>
-          <div className='product'>
-            <img></img>
-            <h3>Snowboard Jacket</h3>
-            <p>Men White Dotted</p>
-            <span className='price'>$329 CAD</span>
-            <button>Add to Cart</button>
+        </section>
+        {/* Footer */}
+        <footer>
+          <div>
+            <h4>Follow us</h4>
+            <ul>
+              <li><a href="#">Twitter</a></li>
+              <li><a href="#">Instagram</a></li>
+              <li><a href="#">Facebook</a></li>
+            </ul>
           </div>
-          <div className='product'>
-            <img></img>
-            <h3>Snowboard Jacket</h3>
-            <p>Men White Dotted</p>
-            <span className='price'>$329 CAD</span>
-            <button>Add to Cart</button>
+          <div>
+            <h4>Help</h4>
+            <ul>
+              <li><a href="#">Help Center</a></li>
+            </ul>
           </div>
-          <div className='product'>
-            <img></img>
-            <h3>Snowboard Jacket</h3>
-            <p>Men White Dotted</p>
-            <span className='price'>$329 CAD</span>
-            <button>Add to Cart</button>
+          <div>
+            <h4>About</h4>
+            <ul>
+              <li><a href="#">Mission Statement</a></li>
+            </ul>
           </div>
-          <div className='product'>
-            <img></img>
-            <h3>Snowboard Jacket</h3>
-            <p>Men White Dotted</p>
-            <span className='price'>$329 CAD</span>
-            <button>Add to Cart</button>
+          <div>
+            <h4>Location</h4>
+            <ul>
+              <li>Canada</li>
+              <li>United States</li>
+              <li>UK</li>
+            </ul>
           </div>
-          <div className='product'>
-            <img></img>
-            <h3>Snowboard Jacket</h3>
-            <p>Men White Dotted</p>
-            <span className='price'>$329 CAD</span>
-            <button>Add to Cart</button>
-          </div>
-        </div>
-      </section>
-      {/* Footer */}
-      <footer>
-        <div>
-          <h4>Follow us</h4>
-          <ul>
-            <li><a href="#">Twitter</a></li>
-            <li><a href="#">Instagram</a></li>
-            <li><a href="#">Facebook</a></li>
-          </ul>
-        </div>
-        <div>
-          <h4>Help</h4>
-          <ul>
-            <li><a href="#">Help Center</a></li>
-          </ul>
-        </div>
-        <div>
-          <h4>About</h4>
-          <ul>
-            <li><a href="#">Mission Statement</a></li>
-          </ul>
-        </div>
-        <div>
-          <h4>Location</h4>
-          <ul>
-            <li>Canada</li>
-            <li>United States</li>
-            <li>UK</li>
-          </ul>
-        </div>
-      </footer>
+        </footer>
 
-    </body>
+      </body>
     </>
   );
 }

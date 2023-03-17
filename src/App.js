30183f783d73c183ac208c8dcee6d2cfc31771ba
Carter Clifford <carterlc@live.ca>
2023-03-16 23:34:28 -0600

    Added a Footer component.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './Header';
 import Hero from './Hero';
 import Cart from './Cart';
 import Product from './Product';
-
+import Footer from './Footer';
 
 function App() {
 
@@ -73,36 +73,7 @@ function App() {
           </div>
         </section>
         {/* Footer */}
-        <footer>
-          <div>
-            <h4>Follow us</h4>
-            <ul>
-              <li><a href="#">Twitter</a></li>
-              <li><a href="#">Instagram</a></li>
-              <li><a href="#">Facebook</a></li>
-            </ul>
-          </div>
-          <div>
-            <h4>Help</h4>
-            <ul>
-              <li><a href="#">Help Center</a></li>
-            </ul>
-          </div>
-          <div>
-            <h4>About</h4>
-            <ul>
-              <li><a href="#">Mission Statement</a></li>
-            </ul>
-          </div>
-          <div>
-            <h4>Location</h4>
-            <ul>
-              <li>Canada</li>
-              <li>United States</li>
-              <li>UK</li>
-            </ul>
-          </div>
-        </footer>
+       <Footer />
     </>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,38 @@
+import './App.css';
+
+export default function Footer() {
+
+    return (
+        <footer>
+            <div>
+                <h4>Follow us</h4>
+                <ul>
+                    <li><a href="#">Twitter</a></li>
+                    <li><a href="#">Instagram</a></li>
+                    <li><a href="#">Facebook</a></li>
+                </ul>
+            </div>
+            <div>
+                <h4>Help</h4>
+                <ul>
+                    <li><a href="#">Help Center</a></li>
+                </ul>
+            </div>
+            <div>
+                <h4>About</h4>
+                <ul>
+                    <li><a href="#">Mission Statement</a></li>
+                </ul>
+            </div>
+            <div>
+                <h4>Location</h4>
+                <ul>
+                    <li>Canada</li>
+                    <li>United States</li>
+                    <li>UK</li>
+                </ul>
+            </div>
+        </footer>
+    )
+}
+
